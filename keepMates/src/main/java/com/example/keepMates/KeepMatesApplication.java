bb6d601cafd8f6fa9573681bd969dee7d533c8d4
tomo <Owner@192.168.40.248>
2026-01-12 12:41:08 +0900

    keepMatesプロジェクト作成

---
+++ keepMates/src/main/java/com/example/keepMates/KeepMatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.keepMates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepMatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepMatesApplication.class, args);
+	}
+
+}
---
+++ keepMates/src/test/java/com/example/keepMates/KeepMatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keepMates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepMatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
